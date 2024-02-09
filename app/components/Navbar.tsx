1db86af2137db02d5242d945b87431afd24fc437
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-09 20:17:25 +0530

    Kinde Auth setup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Themetoggle } from "./Themetoggle";
+import { Button } from "@/components/ui/button";
 
 
 export function Navbar() {
@@ -6,8 +8,17 @@ export function Navbar() {
         <nav className="border-b bg-background h-[10vh] flex items-center">
             <div className="container flex items-center justify-between">
                 <Link href="/">
-                    <h1>Merlin SaaS</h1>
+                    <h1 className="font-bold text-3xl">Merlin SaaS</h1>
                 </Link>
+
+                <div className="flex items-center gap-x-5">
+                    <Themetoggle />
+
+                    <div className="flex items-center gap-x-5">
+                        <Button>Sign In</Button>
+                        <Button variant="secondary">Sign Up</Button>
+                    </div>
+                </div>
             </div>
         </nav>
     )
