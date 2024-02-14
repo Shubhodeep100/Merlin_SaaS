9e827130a4399e776eb0a4c0cc4af569e0078754
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-14 14:21:10 +0530

    billing updates

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,7 +1,46 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { CheckCircle2 } from "lucide-react";
+
+
+
+
+const featureItems = [
+
+    { name: 'Lorem ipsum something' },
+    { name: 'Lorem ipsum something' },
+    { name: 'Lorem ipsum something' },
+    { name: 'Lorem ipsum something' },
+    { name: 'Lorem ipsum something' },
+    { name: 'Lorem ipsum something' },
+]
 export default function BillingPage() {
     return (
-        <div>
-            Hello form thye billing Page
+        <div className="max-w-md mx-auto space-y-4">
+            <Card className="flex flex-col">
+                <CardContent className="py-8">
+                    <div>
+                        <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary">
+                            Monthly
+                        </h3>
+                    </div>
+                    <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+                        $30<span className="ml-1 text-2xl text-muted-foreground">/mo</span>
+                    </div>
+                    <p className="mt-5 text-lg text-muted-foreground">Write as many notes as you want for $30 a Month.</p>
+                </CardContent>
+                <div className="flex-1 flex-col justify-between px-6 pt-6 pb-6 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
+                    <ul className="space-y-4">
+                        {featureItems.map((item, index) => (
+                            <li key={index} className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <CheckCircle2 className="h-6 w-6 text-green-500" />
+                                </div>
+                                <p className="ml-3 text-base">{item.name}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </Card>
         </div>
     )
 }
