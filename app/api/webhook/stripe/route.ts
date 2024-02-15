0498ea9bd7acdd41a1b29d0a2fc8c9f2b47b653b
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-15 11:15:51 +0530

    stripe webhook setup

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,21 +1,28 @@
-import { stripe } from "@/app/lib/stripe"
-import { headers } from "next/headers"
-import Stripe from "stripe"
+import { stripe } from "@/app/lib/stripe";
+import { headers } from "next/headers";
+import Stripe from "stripe";
 
 export async function POST(req: Request) {
-    const body = await req.text()
-    const signature = headers().get('Stripe-Signature') as string
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 
-    let event: Stripe.Event
+  let event: Stripe.Event;
 
-    try {
-        event = stripe.webhooks.constructEvent(
-          body,
-          signature,
-          process.env.STRIPE_WEBHOOK_SECRET as string
-        );
-    } catch (error) {
-        return new Response('webhook error', {status: 400})
-        
-    }
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+  } catch (error) {
+    return new Response("webhook error", { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+  }
 }
