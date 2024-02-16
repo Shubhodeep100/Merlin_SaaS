d72010348c0097ab8f574b52ab80f112f6f8a591
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-17 01:58:46 +0530

    Dashboard data updates

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,71 +1,85 @@
 import { SubmitButton } from "@/app/components/Submitbutton";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import prisma from "@/app/lib/db";
-import { getUser } from "@kinde-oss/kinde-auth-nextjs/dist/types/session/getUser";
-import { error } from "console";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-
+import { unstable_noStore as noStore } from "next/cache";
 
 export default async function NewNoteRoute() {
-
+    noStore();
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
     async function postData(formData: FormData) {
-        "use server"
+        "use server";
+
         if (!user) {
-            throw new Error('Not authorized');
+            throw new Error("Not authorized");
         }
-        const title = formData.get("title") as string
-        const description = formData.get("description") as string
+
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
 
         await prisma.note.create({
             data: {
                 userId: user?.id,
                 description: description,
                 title: title,
-            }
-        })
-        return redirect('/dashboard')
+            },
+        });
+
+        return redirect("/dashboard");
     }
 
-
     return (
-
         <Card>
             <form action={postData}>
                 <CardHeader>
-                    <CardTitle>
-                        New Note
-                    </CardTitle>
+                    <CardTitle>New Note</CardTitle>
                     <CardDescription>
-                        Right here you can create new note.
+                        Right here you can now create your new notes
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="flex flex-col gap-y-5">
                     <div className="gap-y-2 flex flex-col">
                         <Label>Title</Label>
-                        <Input required type="text" placeholder="Title for your Note." />
+                        <Input
+                            required
+                            type="text"
+                            name="title"
+                            placeholder="Title for your note"
+                        />
                     </div>
+
                     <div className="flex flex-col gap-y-2">
                         <Label>Description</Label>
-                        <Textarea name="description" placeholder="Describe your note as you want." required />
+                        <Textarea
+                            name="description"
+                            placeholder="Describe your note as you want"
+                            required
+                        />
                     </div>
                 </CardContent>
+
                 <CardFooter className="flex justify-between">
                     <Button asChild variant="destructive">
-                        <Link href="/dashboard">cancel</Link>
+                        <Link href="/dashboard">Cancel</Link>
                     </Button>
-
                     <SubmitButton />
                 </CardFooter>
             </form>
         </Card>
-    )
+    );
 }
