24f443880f5970b5e474fdba9de805e162bab0fd
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-17 01:13:19 +0530

    Dashboard data updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import prisma from "../lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { File } from "lucide-react";
 
 async function getData(userId: string) {
     const data = await prisma.note.findMany({
@@ -15,9 +17,14 @@ async function getData(userId: string) {
     return data;
 }
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+    const { getUser } = getKindeServerSession()
+    const user = await getUser();
+    const data = await getData(user?.id as string);
+
     return (
-        <div className="grid items-start gap-8">
+        <div className="grid items-start gap-y-8">
             <div className="flex items-center justify-between px-2">
                 <div className="grid gap-1">
                     <h1 className="text-3xl md:text:4xl">Your Notes</h1>
@@ -30,7 +37,22 @@ export default function DashboardPage() {
                         Create a new note.
                     </Link>
                 </Button>
+
+
             </div>
+            {data.length < 1 ? (
+                <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+
+                    <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10"><File className="w-10 h-10 text-primary" /></div>
+                    <h2 className="mt-6 text-xl font-semibold">You dont have any notes created.</h2>
+                    <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">Can see them right here.</p>
+                </div>
+
+            ) : (
+                <div>
+
+                </div>
+            )}
         </div>
     )
 }
