4d759fc5c112c555b816d4727e57d9d7214385fc
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-16 12:31:17 +0530

    Dashboard UI updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function DashboardPage() {
     return (
-        <h1>
-            This is my Dahboard
-        </h1>
+        <div className="grid items-start gap-8">
+            <div className="flex items-center justify-between px-2">
+                <div className="grid gap-1">
+                    <h1 className="text-3xl md:text:4xl">Your Notes</h1>
+                    <p className="text-lg text-muted-foreground">
+                        Here you can see and create notes.
+                    </p>
+                </div>
+                <Button asChild>
+                    <Link href="/dashboard/new">
+                        Create a new note.
+                    </Link>
+                </Button>
+            </div>
+        </div>
     )
 }
