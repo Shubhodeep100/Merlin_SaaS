6675f9039101165302edaf71355783704d282a0e
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-17 01:28:53 +0530

    Dashboard data updates

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,7 +1,31 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function NewNoteRoute() {
     return (
-        <div>
-            <h1>This is from the new page.</h1>
-        </div>
+
+        <Card>
+            <form>
+                <CardHeader>
+                    <CardTitle>
+                        New Note
+                    </CardTitle>
+                    <CardDescription>
+                        Right here you can create new note.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-y-5">
+                    <div className="gap-y-2 flex flex-col">
+                        <Label>Title</Label>
+                        <Input required type="text" placeholder="Title for your Note." />
+                    </div>
+                    <div className="flex flex-col gap-y-2">
+                        <Label>Description</Label>
+                        
+                    </div>
+                </CardContent>
+            </form>
+        </Card>
     )
 }
