281f3bb517798e6c6302d6a3d5b98482baa54845
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-19 19:05:06 +0530

    Delete notes features

--- app/components/Submitbutton.tsx
+++ app/components/Submitbutton.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { Loader2Icon } from "lucide-react";
+import { Loader2, Loader2Icon, Trash } from "lucide-react";
 import { useFormStatus } from "react-dom"
 
 export function SubmitButton() {
@@ -41,6 +41,19 @@ export function StripePortal() {
             {pending ? (<Button disabled className="w-fit">
                 <Loader2Icon className="mr-2 w-4 h-4 animate-spin" />
             </Button>) : (<Button className="w-fit " type="submit">View Payment details</Button>)}
+        </>
+    )
+}
+
+export function TrashDelete() {
+    const { pending } = useFormStatus();
+    return (
+        <>
+            {pending ? (<Button variant="destructive" size="icon">
+                <Loader2 className="h-4 w-4" />
+            </Button>) : (<Button variant="destructive" size="icon">
+                <Trash className="h-4 w-4" />
+            </Button >)}
         </>
     )
 }
