c449768c96b9cbadefa858c234a880f864ddbaba
Shubhodeep <banerjeeshubho98@gmail.com>
2024-02-21 20:40:56 +0530

    cashing updates

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { Themetoggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
-import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { UserNav } from "./UserNav";
 import saas from "@/public/assets/saas.png"
@@ -16,7 +16,7 @@ export async function Navbar() {
             <div className="container flex items-center justify-between">
                 <Link href="/">
                     <div className="flex flex-row gap-1">
-                        <Image src={saas} width={50} height={40} alt="logo" className="transition duration-500 hover:scale-110"/>
+                        <Image src={saas} width={50} height={40} alt="logo" className="transition duration-500 hover:scale-110" />
                         <h1 className="font-bold text-3xl font-signature mt-1">Merlin<span className="text-primary">SaaS</span></h1>
                     </div>
                 </Link>
